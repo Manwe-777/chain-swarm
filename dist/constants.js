2aaf871f07307501fe8e820922a864965053b772
Manwe <manuel.etchegaray7@gmail.com>
2022-07-28 00:43:30 -0300

    mtgatool-db-swarm-v3

--- dist/constants.js
+++ dist/constants.js
@@ -4,5 +4,5 @@ exports.SWARM_KEY = exports.USE_DHT = exports.USE_HTTP = exports.PORT = void 0;
 exports.PORT = process.env.PORT ? parseInt(process.env.PORT) : 8765;
 exports.USE_HTTP = process.env.USE_HTTP || false;
 exports.USE_DHT = process.env.USE_DHT || false;
-exports.SWARM_KEY = process.env.SWARM_KEY || "tool-db-default";
+exports.SWARM_KEY = process.env.SWARM_KEY || "mtgatool-db-swarm-v3";
 //# sourceMappingURL=constants.js.map
