39871a556c71f4a1fadc67662c5a637e95c07605
Manwe <manuel.etchegaray7@gmail.com>
2023-10-08 14:53:02 -0300

    info path and upgrades

--- src/expressSetup.ts
+++ src/expressSetup.ts
@@ -1,10 +1,27 @@
 import { Express } from "express";
 import { ToolDb } from "mtgatool-db";
+import moment from "moment";
+import osu from "node-os-utils";
+
+const startTime = new Date().getTime();
 
 export default function expressSetup(app: Express, db: ToolDb) {
   // Setup Express
   app.get("/", (_req: any, res: any) => {
     res.json({ ok: true, msg: "You found the root!" });
+  });
+
+  app.get("/info", async (_req: any, res: any) => {
+    const currentTime = new Date().getTime();
+    const uptime = currentTime - startTime;
+    const duration = moment.duration(uptime);
+
+    const cpu = osu.cpu;
+
+    const usage = await cpu.usage();
+    const free = await cpu.free();
+
+    res.json({ uptime: duration.humanize(), cpus: cpu.count(), usage, free });
   });
 
   app.get("/peers", (_req: any, res: any) => {
